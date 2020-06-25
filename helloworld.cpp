2c8d9053430092b4dfcacb4360793bfd99a00146
richard <rxue1045@gmail.com>
2020-06-24 18:57:01 -0700

    update

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,9 +1,20 @@
+// Configuration of C++. Need following 2 lines
 #include <iostream>
 
 using namespace std;
 
 int main() {
-  cout << "Hello, world!" << endl;
-  
+  // cout: console out
+  // endl: end line
+
+  // cout << "Hello, world!" << endl;
+  // cout << "My name is Richard." << endl;
+
+  // Drawing a shape
+  cout << "   /｜" << endl;
+  cout << "  / ｜" << endl;
+  cout << " /  ｜" << endl;
+  cout << "/___｜" << endl;
+
   return 0;
 }
