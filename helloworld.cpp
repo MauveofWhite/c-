24da4c5ed82e9b04e5ff97e5b509cf1759d2a7cd
richard <rxue1045@gmail.com>
2020-06-27 15:30:26 -0700

    update

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,5 +1,6 @@
 // Configuration of C++. Need following 2 lines
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
@@ -35,14 +36,43 @@ int main() {
   // cout << "Hello, world! \n"; // \n will start a new line
   // cout << "My name is Richard. \n" << endl;
 
-  // String functions
-  string phrase = "Richard";
-  cout << phrase.length() << endl;
-  cout << phrase[0] << endl;
-  phrase[0] = 'B';
-  cout << phrase[0] << endl;
+  // // String functions
+  // string phrase = "Richard";
+  // cout << phrase.length() << endl;
+  // cout << phrase[0] << endl;
+  // phrase[0] = 'B';
+  // cout << phrase[0] << endl;
+  // // find(str or char, find arg after int elem)
+  // cout << phrase.find("ard", 0) << endl; // returns 4
+  // cout << phrase.find("ard", 4) << endl; // returns 4
+  // cout << phrase.find("ard", 5) << endl; // returns inf (DNE)
+  // // substr(int start index, int length)
+  // string phrasesub = phrase.substr(1, 3);
+  // cout << phrasesub << endl; // returns ich
 
-  cout << phrase.find() << endl;
+  // // Basic C++ mathematics
+  // cout << 5 + 7 * 3 % 2 << endl; // prints 6
+  // cout << 29 / 10 << endl; // prints 2 (floor div between ints)
+  // cout << 5. / 2 << endl; // prints 2.5
+  // double dnum = 3.3;
+  // dnum *= 2;
+  // cout << dnum << endl; // 6.6
+
+  // // Math functions relies <cmath>
+  // cout << pow(2, 5) << endl; // 32, 2 to power of 5
+  // cout << sqrt(36) << endl; // 6
+  // cout << round(4.5) << endl; // 5
+  // cout << floor(4.999999999) << endl; // 4
+  // cout << fmax(3, 10) << endl; // return the larger elem (10)
+  // cout << fmin(3, 10) << endl; // 3. Can only take 2 elems.
+
+  // Prompting
+  int age;
+  cout << "Enter your age: ";
+  cin >> age;
+
+  cout << "You are " << age << " years old." << endl;
+
 
   return 0;
 }
