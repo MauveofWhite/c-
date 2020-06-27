34b47740614828ce29f005aacb674e30957a9a79
richard <rxue1045@gmail.com>
2020-06-27 15:06:59 -0700

    update

--- helloworld.cpp
+++ helloworld.cpp
@@ -22,10 +22,18 @@ int main() {
   // cout << "My name is " << characterName << "." << endl;
   // cout << "I am " << characterAge << " years old." << endl;
 
-  // Data Types
-  
+  // // Data Types
+  // char grade = 'A';
+  // string phrase = "c++ is fun";
+  // int age = 20;
+  // float avg = 90.1
+  // double gpa = -4.5 // can store MORE decimal points than float
+  // boolean isMale = True;
 
-
+  // C++ endl logic
+  cout << "Hello, world!"; // did not start a new line
+  cout << "Hello, world! \n"; // \n will start a new line
+  cout << "My name is Richard. \n" << endl;
 
 
   return 0;
