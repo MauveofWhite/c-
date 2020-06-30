2cebee0931275b5f1c9f2161a8d9ec987d2aaa7a
richard <rxue1045@gmail.com>
2020-06-30 13:24:30 -0700

    update

---
+++ ruby/helloworld.rb
@@ -0,0 +1,12 @@
+# 'print' no skipping line
+print "Hello, print1"
+print "Hello, print2"
+# 'puts' skips a line
+puts "Hello, put1"
+puts "Hello, put2"
+
+# Draw an easy shape
+puts "    /|"
+puts "   / |"
+puts "  /  |"
+puts " /___|"
