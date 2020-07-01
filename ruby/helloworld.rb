f011e8f3c2e7c3ad6f7f346b6ca69c264fb1c2ac
richard <rxue1045@gmail.com>
2020-06-30 20:40:44 -0700

    update

--- ruby/helloworld.rb
+++ ruby/helloworld.rb
@@ -1,12 +1,22 @@
-# 'print' no skipping line
-print "Hello, print1"
-print "Hello, print2"
-# 'puts' skips a line
-puts "Hello, put1"
-puts "Hello, put2"
+# # 'print' no skipping line
+# print "Hello, print1"
+# print "Hello, print2"
+# # 'puts' skips a line
+# puts "Hello, put1"
+# puts "Hello, put2"
+#
+# # Draw an easy shape
+# puts " /___|"
+# puts "    /|"
+# puts "   / |"
+# puts "  /  |"
 
-# Draw an easy shape
-puts "    /|"
-puts "   / |"
-puts "  /  |"
-puts " /___|"
+# # Variables
+# character_name = "Richard"
+# # int can't be inserted between strings
+# character_age = "20"
+# # parethesis is not necessary but recommended
+# puts ("Hello, my name is " + character_name)
+# puts ("I am " + character_age + " years old.")
+
+# Data Types
